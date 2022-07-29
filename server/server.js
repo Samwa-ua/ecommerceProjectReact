2c40f73eaf3ac39cf863dfb77de0ebcd51f663c8
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-07-29 12:08:29 +0300

    feat: Add basic get method for all and single products

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import products from './data/Products.js';
+
+const PORT = 5000;
 
 const app = express();
+
+//Fetch products from mockdata
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+//Fetch single product from mockdata
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(5000, console.log('server running ...'));
+app.listen(PORT, console.log(`server running on port ${PORT}...`));
