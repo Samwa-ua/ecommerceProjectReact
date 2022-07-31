66502d52f7ff23bf21a13dcd1b826daa066bae4e
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-07-31 15:15:05 +0300

    fix: Cleared extencive code

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/Products.js';
 import connectDataBase from './config/MongoDB.js';
 import importData from './DataImport.js';
 import productRoute from './Routes/ProductRouts.js';
@@ -12,7 +11,6 @@ connectDataBase();
 const app = express();
 
 //API
-
 app.use('/api/import', importData);
 app.use('/api/products', productRoute);
 app.use(notFound);
@@ -21,14 +19,3 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`server running on port ${PORT}...`));
-
-// //Fetch products from mockdata
-// app.get('/api/products', (req, res) => {
-//   res.json(products);
-// });
-
-// //Fetch single product from mockdata
-// app.get('/api/products/:id', (req, res) => {
-//   const product = products.find((p) => p._id === req.params.id);
-//   res.json(product);
-// });
