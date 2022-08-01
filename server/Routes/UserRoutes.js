17323e605e2f45a8a56c48982038761779ba4b47
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-08-01 17:24:37 +0300

    feat: Add jwt token functionality

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 import User from '../Models/UserModel.js';
+import generateToken from '../utils/generateToken.js';
 
 const userRouter = express.Router();
 
@@ -15,7 +16,7 @@ userRouter.post(
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
-        token: null,
+        token: generateToken(user._id),
         createdAt: user.createdAt,
       });
     } else {
