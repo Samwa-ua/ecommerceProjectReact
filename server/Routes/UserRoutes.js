dc4a472ca65707c666bd658fe564016aafc2ace9
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-08-01 18:12:26 +0300

    feat: Add user profile API

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
+import protect from '../Middleware/AuthMiddleware.js';
 import User from '../Models/UserModel.js';
 import generateToken from '../utils/generateToken.js';
 
@@ -22,6 +23,26 @@ userRouter.post(
     } else {
       res.status(401);
       throw new Error('Invalid Email or Password');
+    }
+  })
+);
+
+userRouter.get(
+  '/profile',
+  protect,
+  asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        createdAt: user.createdAt,
+      });
+    } else {
+      res.status(404);
+      throw new Error('User not found');
     }
   })
 );
