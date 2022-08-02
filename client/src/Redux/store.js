3a2b6936f235f095447545167aa3ade7d33367d4
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-08-02 11:07:35 +0300

    feat: Add login redux

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -6,21 +6,27 @@ import {
   productListReducer,
 } from './Reducers/ProductReducers';
 import { cartReducer } from './Reducers/CartReducers';
+import { userLoginReducer } from './Reducers/UserReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
-const cartItemsFromLOcalStorage = localStorage.getItem('cartItems')
+const cartItemsFromLocalStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
+const userInfoFromLocalStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
 
 const initialState = {
   cart: {
-    cartItems: cartItemsFromLOcalStorage,
+    cartItems: cartItemsFromLocalStorage,
   },
+  userLogin: { userInfo: userInfoFromLocalStorage },
 };
 
 const middleware = [thunk];
