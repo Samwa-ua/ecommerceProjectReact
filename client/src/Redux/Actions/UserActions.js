6a4d0cc6bb5a3a71eb08542611969b2048b038f2
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-08-02 13:01:16 +0300

    feat: Add logout func

--- client/src/Redux/Actions/UserActions.js
+++ client/src/Redux/Actions/UserActions.js
@@ -3,6 +3,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from '../Constants/UserConstants';
 
 //Login
@@ -32,4 +33,13 @@ export const login = (email, password) => async (dispatch) => {
           : error.message,
     });
   }
+};
+
+//Logout
+
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
+  //optional
+  document.location.href = '/login';
 };
